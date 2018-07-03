6b16d008565211e7338e14f44a13f4b7b57ad58b
Leerng <39564803+Leerng@users.noreply.github.com>
2018-07-02 23:11:49 -0700

    Update Something.java

--- Something.java
+++ Something.java
@@ -10,6 +10,14 @@ public class Something extends JPanel {
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    private void initGUI() {
+        TitleLabel titlelabel = new TitleLabel("Title");
+        add(titleLabel, BorderLayout.PAGE_START);
+        add(centerPanl, BorderLayout.CENTER);
+    }
+    
     public static void main(String[] args) {
         try{
             String className = UIManager.getCrossPlatformLookAndFeelClassName();
